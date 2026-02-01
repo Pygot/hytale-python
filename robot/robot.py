bbc826949562911ed5eb7c676b3c3f2b1b4aa25d
Pygot <rathpavel@icloud.com>
2026-02-02 00:06:03 +0100

    In robot.py, removed Entity and added NPCPlugin for convenience. + Fixed numerous bugs.

--- robot/robot.py
+++ robot/robot.py
@@ -1,46 +1,43 @@
-from com.hypixel.hytale.server.core.asset.type.model.config import ModelAsset, Model
-from com.hypixel.hytale.server.core.universe.world.storage import EntityStore
-from com.hypixel.hytale.server.core.modules.entity.tracker import NetworkId
-from com.hypixel.hytale.server.core.modules.interaction import Interactions
-from com.hypixel.hytale.server.core.modules.entity.component import (
-    TransformComponent,
-    PersistentModel,
-    BoundingBox,
-    ModelComponent
-)
+# Java imports
 from com.hypixel.hytale.server.core.event.events.player import (
     PlayerReadyEvent,
     PlayerDisconnectEvent
 )
-from com.hypixel.hytale.server.core.entity.entities import Player
-from com.hypixel.hytale.component import AddReason, RemoveReason
-from com.hypixel.hytale.server.core.entity import UUIDComponent
-from com.hypixel.hytale.math.vector import Vector3d, Vector3f
-from com.hypixel.hytale.server.npc.entities import NPCEntity
+from com.hypixel.hytale.server.npc import NPCPlugin
+from com.hypixel.hytale.math.vector import Vector3f
 from com.hypixel.hytale.server.core import Message
 from java.io import File, FileReader, FileWriter
 from com.google.gson import Gson
-from java.util import Map, UUID
+from java.util import Map
 
+# Python imports
 from pygot.entity import get_position, get_rotation
 from pygot.world import get_world_by_name
 from pygot.events import register_global
-from pygot.log import error, info, debug
-
 from datetime import datetime
+from pygot.log import Log
 
 import threading, time, os
 
 
 class Robot:
-    def __init__(self, plugin, messages, robot_name="Robot", default_world="default", storage_dir="players_storage/", is_debug=False):
+    def __init__(
+            self,
+            plugin,
+            messages,
+            robot_name="Robot",
+            default_world="default",
+            storage_dir="players_storage/",
+            is_debug=False
+    ):
         # Robot
         self.default_world = default_world
         self.storage_dir = storage_dir
         self.robot_name = robot_name
+        self.log = Log(plugin.Log)
         self.is_debug = is_debug
-        self.logger = plugin.Log
         self.messages = messages
+        self.robot_ref = None
         self.plugin = plugin
         self.gson = Gson()
         self.rot_yaw = 0.0
@@ -69,26 +66,24 @@ class Robot:
 
         # Position
         if position:
-            debug(self.logger, "Updating position of robot & {}".format(player_name), self.is_debug)
+            self.log.debug("Updating position of robot & {}".format(player_name), self.is_debug)
             x, y, z = position.x, position.y, position.z
             self.player_x[player_name], self.x = x, x
             self.player_y[player_name], self.y = y, y
             self.player_z[player_name], self.z = z, z
         else:
-            debug(
-                self.logger,
+            self.log.debug(
                 "Cannot update position for robot & {}! Position not found...".format(player_name), self.is_debug
             )
 
         # Rotation
         if rotation:
-            debug(self.logger, "Updating rotation of robot & {}".format(player_name), self.is_debug)
+            self.log.debug("Updating rotation of robot & {}".format(player_name), self.is_debug)
             x, y = rotation.x, rotation.y
             self.player_rot_yaw[player_name], self.rot_yaw = x, x
             self.player_rot_pitch[player_name], self.rot_y = y, y
         else:
-            debug(
-                self.logger,
+            self.log.debug(
                 "Cannot update rotation for robot & {}! Rotation not found...".format(player_name), self.is_debug
             )
 
@@ -101,12 +96,15 @@ class Robot:
 
         self.player_world[player_name] = get_world_by_name("default")
 
+    def on_leave(self):  # TODO
+        ...
+
     def load_storage(self, player_name):
         file_name = self.storage_dir + player_name.lower().capitalize() + ".json"
 
         file_obj = File(file_name)
         if not file_obj.exists():
-            error(self.logger, "No {} found! Creating...".format(file_name))
+            self.log.error("No {} found! Creating...".format(file_name))
             last_online = datetime.now().isoformat()
             default_data = {
                 "last_online": last_online
@@ -123,8 +121,8 @@ class Robot:
             self.last_online[player_name] = data["last_online"]
             reader.close()
         except Exception as e:
-            debug(self.logger, str(e), self.is_debug)
-            error(self.logger, "Panic attack!")
+            self.log.debug(str(e), self.is_debug)
+            self.log.error("Panic attack!")
 
     def save_storage(self, player_name):
         file_name = self.storage_dir + player_name.lower().capitalize() + ".json"
@@ -135,63 +133,62 @@ class Robot:
         }
 
         if not file_obj.exists():
-            debug(self.logger, "{} file not found while saving data! Creating new...".format(file_name), self.is_debug)
+            self.log.debug(
+                "{} file not found while saving data! Creating new...".format(file_name),
+                self.is_debug
+            )
 
         writer = FileWriter(file_obj)
         writer.write(self.gson.toJson(data))
         writer.close()
 
     def spawn_robot(self):
-        def readd_robot_and_save():
-            world = get_world_by_name(self.default_world)
-            store = world.getEntityStore().getStore()
+        world = get_world_by_name(self.default_world)
+        store = world.getEntityStore().getStore()
 
-            holder = EntityStore.REGISTRY.newHolder()
+        self.log.info("Booted! Waiting for any player...")
+        player_ref = world.getPlayerRefs().iterator().next()
+        self.log.debug("Choosing player {}...".format(player_ref.getUsername()), self.is_debug)
 
-            model_asset = ModelAsset.getAssetMap().getAsset("Minecart")
-            model = Model.createScaledModel(model_asset, 10.0)
+        transform = player_ref.getTransform()
+        result = NPCPlugin.get().spawnNPC(
+            store,
+            "Horse",
+            None,
+            transform.getPosition(),
+            Vector3f(0, 0, 0)
+        )
 
-            position = Vector3d(-596, 122.0, -152.0)
-            rotation = Vector3f(0.0, 0.0)
-
-            holder.addComponent(TransformComponent.getComponentType(), TransformComponent(position, rotation))
-            holder.addComponent(PersistentModel.getComponentType(), PersistentModel(model.toReference()))
-            holder.addComponent(ModelComponent.getComponentType(), ModelComponent(model))
-            holder.addComponent(BoundingBox.getComponentType(), BoundingBox(model.getBoundingBox()))
-            holder.addComponent(NetworkId.getComponentType(), NetworkId(store.getExternalData().takeNextNetworkId()))
-            holder.addComponent(Interactions.getComponentType(), Interactions())
-            holder.ensureComponent(UUIDComponent.getComponentType())
-
-            store.addEntity(holder, AddReason.SPAWN)
-
-        self.plugin.runOnMain(readd_robot_and_save)
-        info(self.logger, "Booted!")
+        if result:
+            self.log.info("Successfully spawned {}!".format(self.robot_name))
+            self.robot_ref = result.first()
 
     def on_leave(self, event):
         player_name = event.getPlayerRef().getUsername()
         self.save_storage(player_name)
-        # In-memory cleanup - would cause RAM go up quickly after a few joins and leaves.
-        del self.player_x[player_name]
-        del self.player_y[player_name]
-        del self.player_z[player_name]
-        del self.last_online[player_name]
-        del self.player_rot_yaw[player_name]
-        del self.player_rot_pitch[player_name]
-        del self.player_world[player_name]
+        try:
+            # In-memory cleanup - would cause RAM go up quickly after a few joins and leaves.
+            del self.player_x[player_name]
+            del self.player_y[player_name]
+            del self.player_z[player_name]
+            del self.last_online[player_name]
+            del self.player_rot_yaw[player_name]
+            del self.player_rot_pitch[player_name]
+            del self.player_world[player_name]
+        except KeyError:
+            pass
 
     def on(self):
-        info(self.logger, "Booting...")
+        self.log.info("Booting...")
 
         try:
             os.mkdir(self.storage_dir)
         except:
             pass
 
-        debug(self.logger, "Registering events...", self.is_debug)
+        self.log.debug("Registering events...", self.is_debug)
         register_global(self.plugin, PlayerReadyEvent, self.on_join)
         register_global(self.plugin, PlayerDisconnectEvent, self.on_leave)
 
-        # Run everything else outside the main thread to prevent blockage
-        robot_thread = threading.Thread(target=self.spawn_robot)
-        robot_thread.setDaemon(True)
-        robot_thread.start()
+        # Run spawn_robot in main thread exposed by .jar as plugin.runOnMain
+        self.plugin.runOnMain(self.spawn_robot)
