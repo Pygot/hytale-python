4f55ff6048e1a754e2044fa3ddd08072a2390d2d
Pygot <rathpavel@icloud.com>
2026-02-02 10:38:08 +0100

    Fixed numerous bugs, duplicated method and added cleaning NPCs, might have to find better solution for cleaning tho.

--- robot/robot.py
+++ robot/robot.py
@@ -1,12 +1,16 @@
 # Java imports
+from com.hypixel.hytale.server.core.command.system import CommandManager
 from com.hypixel.hytale.server.core.event.events.player import (
     PlayerReadyEvent,
     PlayerDisconnectEvent
 )
+from com.hypixel.hytale.server.core.console import ConsoleSender
+from com.hypixel.hytale.component import RemoveReason
 from com.hypixel.hytale.server.npc import NPCPlugin
 from com.hypixel.hytale.math.vector import Vector3f
 from com.hypixel.hytale.server.core import Message
 from java.io import File, FileReader, FileWriter
+from java.lang import IllegalStateException
 from com.google.gson import Gson
 from java.util import Map
 
@@ -31,12 +35,13 @@ class Robot:
             is_debug=False
     ):
         # Robot
+        self.cmd_manager = CommandManager.get()
+        self.log = Log(plugin.Log, is_debug)
         self.default_world = default_world
         self.storage_dir = storage_dir
         self.robot_name = robot_name
-        self.log = Log(plugin.Log)
-        self.is_debug = is_debug
         self.messages = messages
+        self.robot_world = None
         self.robot_ref = None
         self.plugin = plugin
         self.gson = Gson()
@@ -66,25 +71,25 @@ class Robot:
 
         # Position
         if position:
-            self.log.debug("Updating position of robot & {}".format(player_name), self.is_debug)
+            self.log.debug("Updating position of robot & {}".format(player_name))
             x, y, z = position.x, position.y, position.z
             self.player_x[player_name], self.x = x, x
             self.player_y[player_name], self.y = y, y
             self.player_z[player_name], self.z = z, z
         else:
             self.log.debug(
-                "Cannot update position for robot & {}! Position not found...".format(player_name), self.is_debug
+                "Cannot update position for robot & {}! Position not found...".format(player_name)
             )
 
         # Rotation
         if rotation:
-            self.log.debug("Updating rotation of robot & {}".format(player_name), self.is_debug)
+            self.log.debug("Updating rotation of robot & {}".format(player_name))
             x, y = rotation.x, rotation.y
             self.player_rot_yaw[player_name], self.rot_yaw = x, x
             self.player_rot_pitch[player_name], self.rot_y = y, y
         else:
             self.log.debug(
-                "Cannot update rotation for robot & {}! Rotation not found...".format(player_name), self.is_debug
+                "Cannot update rotation for robot & {}! Rotation not found...".format(player_name)
             )
 
         player.sendMessage(
@@ -95,9 +100,6 @@ class Robot:
         )
 
         self.player_world[player_name] = get_world_by_name("default")
-
-    def on_leave(self):  # TODO
-        ...
 
     def load_storage(self, player_name):
         file_name = self.storage_dir + player_name.lower().capitalize() + ".json"
@@ -121,8 +123,8 @@ class Robot:
             self.last_online[player_name] = data["last_online"]
             reader.close()
         except Exception as e:
-            self.log.debug(str(e), self.is_debug)
             self.log.error("Panic attack!")
+            self.log.debug(str(e))
 
     def save_storage(self, player_name):
         file_name = self.storage_dir + player_name.lower().capitalize() + ".json"
@@ -134,8 +136,7 @@ class Robot:
 
         if not file_obj.exists():
             self.log.debug(
-                "{} file not found while saving data! Creating new...".format(file_name),
-                self.is_debug
+                "{} file not found while saving data! Creating new...".format(file_name)
             )
 
         writer = FileWriter(file_obj)
@@ -143,25 +144,36 @@ class Robot:
         writer.close()
 
     def spawn_robot(self):
-        world = get_world_by_name(self.default_world)
-        store = world.getEntityStore().getStore()
+        self.robot_world = get_world_by_name(self.default_world)
 
         self.log.info("Booted! Waiting for any player...")
-        player_ref = world.getPlayerRefs().iterator().next()
-        self.log.debug("Choosing player {}...".format(player_ref.getUsername()), self.is_debug)
+        while self.robot_world.getPlayerCount() < 1:  # Optional, it might block /plugin command
+            time.sleep(0.25)
 
-        transform = player_ref.getTransform()
-        result = NPCPlugin.get().spawnNPC(
-            store,
-            "Horse",
-            None,
-            transform.getPosition(),
-            Vector3f(0, 0, 0)
-        )
+        def spawn():
+            player_ref = self.robot_world.getPlayerRefs().iterator().next()
+            self.log.debug("Choosing player {}...".format(player_ref.getUsername()))
 
-        if result:
-            self.log.info("Successfully spawned {}!".format(self.robot_name))
-            self.robot_ref = result.first()
+            transform = player_ref.getTransform()
+            result = NPCPlugin.get().spawnNPC(
+                self.robot_world.getEntityStore().getStore(),
+                "Horse",
+                None,
+                transform.getPosition(),
+                Vector3f(0, 0, 0)
+            )
+
+            if result:
+                self.log.info("Successfully spawned {}!".format(self.robot_name))
+                self.robot_ref = result.first()
+
+        if not self.robot_ref:
+            # Clean all NPCs to remove any Robot leftovers. Optional and not an optimal solution
+            # Cannot be called in the main thread!
+            self.log.debug("Cleaning all NPCs...")
+            self.cmd_manager.handleCommand(ConsoleSender.INSTANCE, "npc clean --world={} --confirm".format(self.default_world))
+            # Run spawn() in the main thread exposed by .jar as plugin.runOnMain
+            self.plugin.runOnMain(spawn)
 
     def on_leave(self, event):
         player_name = event.getPlayerRef().getUsername()
@@ -186,9 +198,10 @@ class Robot:
         except:
             pass
 
-        self.log.debug("Registering events...", self.is_debug)
+        self.log.debug("Registering events...")
         register_global(self.plugin, PlayerReadyEvent, self.on_join)
         register_global(self.plugin, PlayerDisconnectEvent, self.on_leave)
 
-        # Run spawn_robot in main thread exposed by .jar as plugin.runOnMain
-        self.plugin.runOnMain(self.spawn_robot)
+        robot_thread = threading.Thread(target=self.spawn_robot)
+        robot_thread.setDaemon(True)
+        robot_thread.start()
