5ca4b2760a9f577667b37db42ae2fc96f8fbd74c
Pygot <rathpavel@icloud.com>
2026-02-02 19:17:19 +0100

    I don't even know anymore, half of the idea is missing.

--- robot/config.py
+++ robot/config.py
@@ -4,5 +4,13 @@ robot_name = "Robot"
 
 messages = {
     "on_join": "Hello {}!",
+    "hi": "Hello {}!",
     "prefix": "{}: ".format(robot_name),
 }
+
+# Was trying to do it modular but not enough time. Improvising...
+enabled_commands = [
+    "tp",
+    "hi",
+    "die"
+]
--- robot/main.py
+++ robot/main.py
@@ -1,5 +1,10 @@
-from config import messages, is_debug
+from config import messages, is_debug, enabled_commands
 from robot import Robot
 
 if __name__ == "__main__":
-    Robot(plugin=plugin, messages=messages, is_debug=is_debug).on()
+    Robot(
+        plugin=plugin,
+        messages=messages,
+        enabled_cmds=enabled_commands,
+        is_debug=is_debug
+    ).on()
